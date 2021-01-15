c223b58b0bc54f268a95ce3ad7dc369a8679de93
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2021-01-15 11:39:34 +0100

    data writer

--- sugarscape/main.py
+++ sugarscape/main.py
@@ -2,9 +2,10 @@
 from agents import Consumer, Sugar
 from model import SugarModel
 
+import csv
 
 def main():
-    global data
+    # global data
     N = 100
     size = 50
     steps = 100
@@ -13,6 +14,11 @@ def main():
     for i in range(steps):
         model.step() 
     data = model.datacollector.get_agent_vars_dataframe()
+    
+    with open('data/data.csv', mode='w') as file:
+        data_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in data:
+            data_writer.writerow(row)
 
     
 if __name__ == "__main__":
