1ba7e48f59e6f02198d816b0504d305a5c56dce4
Lars-Water <54835307+Lars-Water@users.noreply.github.com>
2021-01-30 10:34:34 +0100

    Update model_viz.py

--- sugarscape/model_viz.py
+++ sugarscape/model_viz.py
@@ -40,7 +40,7 @@ class HistogramModule(VisualizationElement):
 
 N = 400
 size = 50
-model = SugarModel(N, width=size, height=size, amsterdam_map = False)
+model = SugarModel(N, width=size, height=size, amsterdam_map = True)
 
 # Create a visualized grid of 50 by 50 cells, and display it as 800 by 800 pixels
 grid = CanvasGrid(model.agent_portrayal, size, size, 700, 700)
